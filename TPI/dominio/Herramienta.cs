9ab7746f79271231e4771eb09cd9374875753e2f
Juang710 <juangt965@gmail.com>
2022-09-20 22:06:18 -0300

    clases Herramienta y Tipo_Herramienta agregadas

--- TPI/dominio/Herramienta.cs
+++ TPI/dominio/Herramienta.cs
@@ -8,11 +8,18 @@ namespace TPI.dominio
 {
     internal class Herramienta
     {
-        public string Marca_Herramienta { get; set; }
-        public string Modelo_Herramienta { get; set; }
-        public Tipo_Herramienta Tipo_Herramienta { get; set; }
-        public Proveedor Proveedor_Herramienta { get; set; }
-
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public Tipo_Herramienta Tipo { get; set; }
+        public Proveedor Proveedor { get; set; }
+        public int Vida_Util { get; set; }
+        public Herramienta(string marca, string mod, Tipo_Herramienta tipo, Proveedor prov, int vid)
+        {
+            Marca = marca;
+            Modelo = mod;
+            Tipo = tipo;
+            Proveedor = prov;
+            Vida_Util = vid;
+        }
     }
 }
-sqlConnection
--- TPI/dominio/Tipo_Herramienta.cs
+++ TPI/dominio/Tipo_Herramienta.cs
@@ -2,5 +2,14 @@
 {
     public class Tipo_Herramienta
     {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Tipo_Herramienta(int cod, string nom, string des)
+        {
+            Codigo = cod;
+            Nombre = nom;
+            Descripcion = des;
+        }
     }
 }
