c4e5015313700bdfa6e1f1911c13174fa3733723
Juang710 <juangt965@gmail.com>
2022-09-20 23:15:29 -0300

    ...

---
+++ TPI/dominio/Composicion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPI.dominio
+{
+    internal class Composicion
+    {
+        public Material Material { get; set; }
+        public Material Material_Componente { get; set; }
+        public int Porcentaje { get; set; }
+        public Composicion(Material material, Material material_Componente, int porcentaje)
+        {
+            Material = material;
+            Material_Componente = material_Componente;
+            Porcentaje = porcentaje;
+        }
+    }
+}
---
+++ TPI/dominio/Repuesto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPI.dominio
+{
+    internal class Repuesto
+    {
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public string Tipo { get; set; }
+        public int Cantidad { get; set; }
+        public Repuesto(string modelo, string marca, string tipo, int cantidad)
+        {
+            Modelo = modelo;
+            Marca = marca;
+            Tipo = tipo;
+            Cantidad = cantidad;
+        }   
+    }
+}
