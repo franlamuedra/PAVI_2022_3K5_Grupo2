d88ecd1c2831a91580a298aa2243a05fd5947083
Lautaro <lautaromazurek@gmail.com>
2022-10-15 17:24:02 -0300

    3 ABMC

---
+++ TPI/dominio/Mantenimiento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPI.dominio
+{
+    public class Mantenimiento
+    {
+        public int Numero_Mantenimiento { get; set; }
+        public int Codigo_Herramienta { get; set; }
+        public string Fecha { get; set; }
+        public int Codigo_Empleado { get; set; }
+        public string Cambio { get; set; }
+
+        public Mantenimiento()
+        {
+
+        }
+    }
+}
